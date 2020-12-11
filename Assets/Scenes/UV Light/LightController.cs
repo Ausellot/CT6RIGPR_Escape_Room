40b74a2440843270773e755aeb971ad51fd540a9
Lawrence <lawrencearoble@gmail.com>
2020-12-11 11:21:44 +0000

    Flashlight + UV Light (Needs some more work)

---
+++ Assets/Scenes/UV Light/LightController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightController : MonoBehaviour
+{
+    public Light flashlight;
+    public Material reveal;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       reveal.SetVector("_LightPosition", flashlight.transform.position);
+        reveal.SetVector("_LightDirection", -flashlight.transform.forward);
+        reveal.SetFloat("_LightAngle", flashlight.spotAngle); 
+
+        if(Input.GetKeyDown(KeyCode.F) && flashlight.enabled == false)
+        {
+            flashlight.enabled = true;
+            
+        
+
+
+        } else if(Input.GetKeyDown(KeyCode.F) && flashlight.enabled == true)
+        {
+            flashlight.enabled = false;
+        }
+    }
+}
