b958640882f63f91ad58552cd4eb8104dcd3a1cb
Lawrence <lawrencearoble@gmail.com>
2020-12-11 11:26:24 +0000

    Quality of Life

--- Assets/Scenes/UV Light/LightController.cs
+++ Assets/Scenes/UV Light/LightController.cs
@@ -12,7 +12,7 @@ public class LightController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        flashlight.enabled = false;
     }
 
     // Update is called once per frame
