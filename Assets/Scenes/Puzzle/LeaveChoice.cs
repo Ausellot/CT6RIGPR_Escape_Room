b0b7904640feb9981ba0c8bc4e72085b455ce81f
JosephFBatchelor <Joseph.batchelor@talktalk.net>
2020-12-17 14:03:58 +0000

    Final Choices Added

---
+++ Assets/Scenes/Puzzle/LeaveChoice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveChoice : MonoBehaviour
+{
+    public GameObject LeaveChoiceInstructions;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "LeaveChoice")
+        {
+            LeaveChoiceInstructions.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "LeaveChoice")
+        {
+            LeaveChoiceInstructions.SetActive(false);
+        }
+    }
+}
