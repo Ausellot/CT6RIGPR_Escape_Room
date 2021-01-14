ced3542e677db43025bb30c1a701b620e9096360
JosephFBatchelor <Joseph.batchelor@talktalk.net>
2021-01-14 14:57:09 +0000

    SafePuzzle

---
+++ Assets/Scenes/Puzzle/Safe.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Safe : MonoBehaviour
+{
+    public GameObject Enable;
+    public GameObject SafeDoor1;
+
+    public string CorrectPassword = "123";
+    public string input;
+    public Text DisplayText;
+    public AudioSource CorrectSound;
+    public AudioSource WrongSound;
+
+    private bool KeypadScreen;
+    private float btnClicked = 0;
+    private float NumOfGuesses;
+
+    void Start()
+    {
+        btnClicked = 0;
+        NumOfGuesses = CorrectPassword.Length;
+    }
+
+    void Update()
+    {
+        if (btnClicked == NumOfGuesses)
+        {
+            if (input == CorrectPassword)
+            {
+                SafeDoor1.GetComponent<Animator>().Play("SafeOpen");
+                CorrectSound.Play();
+                Debug.Log("Correct");
+                input = "";
+                btnClicked = 0;
+            }
+            else
+            {
+                WrongSound.Play();
+                input = "";
+                DisplayText.text = input.ToString();
+                btnClicked = 0;
+            }
+        }
+    }
+    void OnGUI()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                var selction = hit.transform;
+
+                if (selction.CompareTag("Safe"))
+                {
+                    KeypadScreen = true;
+
+                    var selectionRender = selction.GetComponent<Renderer>();
+                    if (selectionRender != null)
+                    {
+                        KeypadScreen = true;
+                    }
+                }
+            }
+        }
+
+        if (KeypadScreen)
+        {
+            Enable.SetActive(true);
+            Time.timeScale = 0.0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void ValueEntered(string valueEntered)
+    {
+        switch (valueEntered)
+        {
+            case "Q":
+                Enable.SetActive(false);
+                btnClicked = 0;
+                input = "";
+                KeypadScreen = false;
+                DisplayText.text = input.ToString();
+                Time.timeScale = 1.0f;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                break;
+
+            case "C":
+                btnClicked = 0;
+                input = "";
+                DisplayText.text = input.ToString();
+                break;
+
+            default:
+                btnClicked++;
+                input += valueEntered;
+                DisplayText.text = input.ToString();
+                break;
+        }
+    }
+}
+
