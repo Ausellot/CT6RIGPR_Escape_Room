9a26e652bca21f5d383f25e7e9f5aba706cf817f
UP904051 <up904051@myport.ac.uk>
2021-03-03 12:32:50 +0000

    KeyFalling
    Fixed the bug where the key would hang in the air for a few moments after power has been turned on. Additionally it prevents the key from being knocked down with a torch before turning on the power.

---
+++ Assets/Scenes/Room Management/Scr_KeyFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_KeyFall : MonoBehaviour
+{
+
+    public GameObject KeyToDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (KeyToDrop != null) { 
+
+        if (transform.rotation.eulerAngles.y >= 286)
+        {           
+            KeyToDrop.GetComponent<BoxCollider>().enabled = true;
+            KeyToDrop.GetComponent<Rigidbody>().useGravity = true;
+        }
+      }
+    }
+}
