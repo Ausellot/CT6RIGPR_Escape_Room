785df79a0a97e2e95a6afc55e8e0f7f94a1f030c
Lawrence <lawrencearoble@gmail.com>
2021-03-17 20:20:25 +0000

    Update CameraMovement.cs

--- Assets/Scenes/Player/CameraMovement.cs
+++ Assets/Scenes/Player/CameraMovement.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     float rotX;
-    public float mouseSen = 400f;
+    public float mouseSen;
     public Camera cam;
     public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (mouseSen == 0)
+        if(mouseSen == 0)
         {
             mouseSen = Settings.camSen;
         }
+        
 
         Cursor.lockState = CursorLockMode.Locked;
     }
