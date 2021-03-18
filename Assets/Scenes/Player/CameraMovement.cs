44fce27b3bd13ac02824dc21854f7f456ae9ed28
Lawrence <lawrencearoble@gmail.com>
2021-03-18 18:08:39 +0000

    Update CameraMovement.cs

--- Assets/Scenes/Player/CameraMovement.cs
+++ Assets/Scenes/Player/CameraMovement.cs
@@ -12,7 +12,6 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         mouseSen = Settings.camSen;
         cam.fieldOfView = Settings.fov;
 
